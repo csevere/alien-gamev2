dcfb743ddc4aca8eb82e5cf33cc7a5ec5f403600
csevere <carla.severe@gmail.com>
2017-12-02 19:14:19 -0500

    setting up redux and json pull

---
+++ src/Components/SceneCards/SceneCard1.js
@@ -0,0 +1,57 @@
+import React, {Component} from 'react';
+import { Container, Row, Col, Button, Card, CardBlock, CardText, CardImg, CardImgOverlay} from 'reactstrap';
+import {Link} from 'react-router-dom'; 
+
+class SceneCard1 extends Component{
+    renderScene(){
+        return this.props.storytextlibrary.map((storytext) => {
+            return(
+                <Container>
+                    <Row>
+                        <Card className = "scene-card" inverse>
+                            <CardImg className = "img-fluid" top width="100%" src={storytext.image} alt="Card image cap" />
+                            <CardImgOverlay className = "scene-bottom">
+                                <CardText className = "scene-text">
+                                    {storytext.text}
+                                </CardText>
+                            </CardImgOverlay>
+                        </Card> 
+                    </Row> 
+                    <Row className = "scene-button float-right">
+                        <Button className = "btn btn-dark">NEXT</Button>
+                    </Row>  
+                </Container>
+
+            )
+
+        });
+    }
+
+    render(){
+        return(
+            <div>
+                <div className = "scene-wrapper no-gutters">
+                    <Container>
+                        <Row>
+                            <Card className = "scene-card" inverse>
+                                <CardImg className = "img-fluid" top width="100%" src="Images/spaceship_interior.jpg" alt="Card image cap" />
+                                <CardImgOverlay className = "scene-bottom">
+                                    <CardText className = "scene-text">
+                                        Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet,
+                                    </CardText>
+                                </CardImgOverlay>
+                            </Card> 
+                        </Row> 
+                        <Row className = "scene-button float-right">
+                            <Button className = "btn btn-dark">NEXT</Button>
+                        </Row>  
+                    </Container>
+                </div>
+            </div>  
+        )
+    }
+}
+
+export default SceneCard1; 
+
+//allow only 65 words per scene text 
--- src/PrimaryLayout.js
+++ src/PrimaryLayout.js
@@ -3,7 +3,7 @@ import { Route } from 'react-router-dom';
 import Welcome from './Components/Welcome';
 import Play from './Components/Play'; 
 import Footer from './Components/Footer';
-import Scene from './Components/Scene';  
+import SceneBoard from './Components/SceneBoard';  
 
 
 const PrimaryLayout = () => (
@@ -11,7 +11,7 @@ const PrimaryLayout = () => (
       <main>
         <Route path="/" exact component={Welcome} />
         <Route path="/play" component={Play} />
-        <Route path ="/scene" component = {Scene}/>
+        <Route path ="/scene" component = {SceneBoard}/>
         <Footer/>
       </main>
     </div>
