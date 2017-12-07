11faf844b263d92c3f973c0f3b697479c7c07240
Carla Severe <carlaflagco@gmail.com>
2017-12-07 16:49:17 -0500

    more updates

--- src/Components/ConversationCards/Conversation1.js
+++ src/Components/ConversationCards/Conversation1.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import { Container, Row, Col, Button, Card, CardBlock, CardText, CardImg, CardImgOverlay} from 'reactstrap';
+import { Container, Row, Col, Button, Card, CardHeader, CardBlock, CardText, CardImg, CardImgOverlay} from 'reactstrap';
 import {Link} from 'react-router-dom'; 
 import { connect} from 'react-redux';
 import * as actions from '../../Actions/story_actions';
@@ -17,18 +17,15 @@ class Conversation1 extends Component{
             <div>
                 <div className = "convo-wrapper no-gutters">
                     <Container className = "position-relative">
-                        <Card  className = "scene-card position-absolute" inverse>
-                            <CardImg className = "img-fluid" top width="100%" alt="Card image cap" />
-                            <CardImgOverlay className = "scene-bottom">
-                                <div className = "float-right place">{storyscene.place}</div> 
-                                <Typing speed = {100} loop = {true}>    
-                                    <CardText className = "scene-text"> 
-                                        {storyscene.text}
-                                    </CardText> 
-                                </Typing> 
-                            </CardImgOverlay>
-                        </Card> 
-                        <Row style = {ContinueStyle} className = "no-gutters">
+                        <Card>
+                            <CardHeader>Ryker</CardHeader>
+                                <CardBlock>
+                                    <CardText>With supporting text below as a natural lead-in to additional content.</CardText>
+                                </CardBlock>
+                        </Card>
+
+
+                        <Row className = "no-gutters">
                             <Col md="5"  className="align-middle">
                                 <Link to = "/convo"><Button className = "continue" color="dark" size="lg" block>CONTINUE</Button></Link>
                             </Col>
@@ -43,7 +40,5 @@ class Conversation1 extends Component{
         )
     }
 }
-
-
 
 export default Conversation1; 
