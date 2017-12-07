e5885056fab708a9ee40de85a8ff0ba766113274
Carla Severe <carlaflagco@gmail.com>
2017-12-07 16:00:35 -0500

    updates

--- src/Components/ConversationCards/Conversation1.js
+++ src/Components/ConversationCards/Conversation1.js
@@ -15,7 +15,30 @@ class Conversation1 extends Component{
 
         return(
             <div>
-                <h1 className = "text-dark">hello</h1>
+                <div className = "convo-wrapper no-gutters">
+                    <Container className = "position-relative">
+                        <Card  className = "scene-card position-absolute" inverse>
+                            <CardImg className = "img-fluid" top width="100%" alt="Card image cap" />
+                            <CardImgOverlay className = "scene-bottom">
+                                <div className = "float-right place">{storyscene.place}</div> 
+                                <Typing speed = {100} loop = {true}>    
+                                    <CardText className = "scene-text"> 
+                                        {storyscene.text}
+                                    </CardText> 
+                                </Typing> 
+                            </CardImgOverlay>
+                        </Card> 
+                        <Row style = {ContinueStyle} className = "no-gutters">
+                            <Col md="5"  className="align-middle">
+                                <Link to = "/convo"><Button className = "continue" color="dark" size="lg" block>CONTINUE</Button></Link>
+                            </Col>
+                        </Row>
+                        <Row className = "scene-button float-right">
+                            <Button  className = "btn btn-dark float-left">BACK</Button>
+                            <Button  className = "btn btn-dark float-right">NEXT</Button>
+                        </Row> 
+                    </Container> 
+                </div>
             </div>  
         )
     }
